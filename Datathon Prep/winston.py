f78ddeb7fa45966e277583e31fae524e26ac96e1
Winston Ta <winstonnta@gmail.com>
2025-03-08 12:50:09 -0800

    compare yearstart and yearend values in winston.py

--- Datathon Prep/winston.py
+++ Datathon Prep/winston.py
@@ -6,3 +6,6 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv(r'D:\08 Skillset\Programming\Machine-Learning\Datathon Prep\train.csv')
 print(df.head())
+
+# compare if all YearStart and all YearEnd values are the same
+print((df['YearStart'] == df['YearEnd']).all())
