54dfb98edffc192255d4ec2aed063561ffa8601c
Mindy Yun <myyun48@gmail.com>
2025-03-07 23:13:37 -0800

    install the libraries

--- Datathon Prep/mindy.py
+++ Datathon Prep/mindy.py
@@ -1,1 +1,11 @@
 """Mindy's Python File"""
+# Import the modules, read the dataset and create a Pandas DataFrame.
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+mall_df = pd.read_csv("https://s3-student-datasets-bucket.whjr.online/whitehat-ds-datasets/Mall_Customers.csv")
+
+# Print the first five records
+mall_df.head()
