1329c1a51c85a4c2db6282a5819db42b1bd95135
Winston Ta <winstonnta@gmail.com>
2025-03-08 10:51:25 -0800

    winston vs code test + df code life hack

--- Datathon Prep/main.py
+++ Datathon Prep/main.py
@@ -3,3 +3,6 @@
 "my name is jason and this will work"
 "my name is mindy and this will work better than jason's"
 "hello my name is winston and im the best one of them all"
+
+"""test 2 columns are same instantly"""
+# (df['YearStart'] == df['YearEnd']).all()
--- Datathon Prep/winston.py
+++ Datathon Prep/winston.py
@@ -1,1 +1,8 @@
 """Winston's Python File"""
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r'D:\08 Skillset\Programming\Machine-Learning\Datathon Prep\train.csv')
+print(df.head())
