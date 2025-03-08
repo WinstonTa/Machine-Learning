f8e4e29f38f9c4e8bfb5efbd9bc4df638046fc89
Winston Ta <winstonnta@gmail.com>
2025-03-08 08:32:10 -0800

    wonton da best

--- Datathon Prep/main.py
+++ Datathon Prep/main.py
@@ -2,3 +2,4 @@
 "hello my name is jason"
 "my name is jason and this will work"
 "my name is mindy and this will work better than jason's"
+"hello my name is winston and im the best one of them all"
